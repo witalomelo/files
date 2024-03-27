d3130ea0db8e7ba3515bd1b1875cf9b2b6bf2cd6
witalomelo <witalovmelo@gmail.com>
2024-03-27 11:25:45 -0300

    Utilizando o FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,26 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.BufferOverflowException;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Program {
     public static void main(String[] args) {
 
-        String path = "/home/witalo/Documentos/in.txt";
+        String[] lines = new String[]{"good morning", "good afternoon","good nigth"};
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        String path = "/home/witalo/Documentos/out.txt";
 
-            String line = br.readLine();
-
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
             }
-
         } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
+
 
     }
 
