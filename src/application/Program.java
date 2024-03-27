c9990172e1a154b5e772e9082a1a37d3e1f2cc1d
witalomelo <witalovmelo@gmail.com>
2024-03-27 11:09:13 -0300

    Fechando as streams de forma manual

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,45 @@
 package application;
 
-import java.io.File;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Program {
     public static void main(String[] args) {
 
-        File file = new File("/home/witalo/Documentos/int.txt");
-        Scanner sc = null;
+        String path = "/home/witalo/Documentos/in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
 
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            if(sc != null){
-                sc.close();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
             }
+
         }
 
 
