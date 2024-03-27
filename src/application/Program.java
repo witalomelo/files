c413b64dbad1c732e0a90d30013e8f880d787d9b
witalomelo <witalovmelo@gmail.com>
2024-03-27 11:13:55 -0300

    Utilizando o bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,18 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.BufferOverflowException;
 
 
 public class Program {
     public static void main(String[] args) {
 
         String path = "/home/witalo/Documentos/in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
 
@@ -26,22 +23,10 @@ public class Program {
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-
-            }
-
         }
 
-
     }
+
+
 }
+
